27a34a9c10f632dcc2c75ff268b819ddc0f51ddc
ales <alesjanko.info@gmail.com>
2022-05-06 14:12:39 -0500

    more changes

--- graph.h
+++ graph.h
@@ -1,6 +1,12 @@
 #ifndef __GRAPH__
 #define __GRAPH__
 
+#include "gthread/glthread.h"
+
+#define IF_NAME_SIZE    10
+#define NODE_NAME_SIZE  10
+#define MAX_IF_PER_NODE  4
+
 /********************************************************/
 /*                                                      */
 /*  node_t                                              */
@@ -18,20 +24,37 @@
 /*                                                      */
 /********************************************************/
 
-typedef struct node_t
-{
+/* Foward declarations */
+typedef struct node_t node;
+typedef struct interface_t interface;
+typedef struct link_t link;
+typedef struct graph_t graph;
 
+typedef struct graph_t
+{
+    char topology_name[32];
+    glthread_t node_list; 
 };
 
-typedef struct interface_t
+typedef struct node_t
 {
-    
+    char name[NODE_NAME_SIZE];
+    interface * interfaces[MAX_IF_PER_NODE];
+    glthread_t grapg_glue;
 };
 
 typedef struct link_t
 {
-    
+    interface if_1;
+    interface if_2;
+    unsigned int cost;
 };
 
+typedef struct interface_t
+{
+    char name[IF_NAME_SIZE];
+    node *owning_node;
+    link *link;    
+};
 
 #endif // __GRAPH__
